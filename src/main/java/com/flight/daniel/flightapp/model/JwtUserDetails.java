8f9fd89fc6f7e9b4f8457235a9f78d7061865446
oheyy <daniel.seunghyun.oh@gmail.com>
2020-06-13 12:37:47 +1000

    Fix UserDetails

--- src/main/java/com/flight/daniel/flightapp/model/JwtUserDetails.java
+++ src/main/java/com/flight/daniel/flightapp/model/JwtUserDetails.java
@@ -21,36 +21,40 @@ public class JwtUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return userName;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
+    }
+
+    public String getToken(){
+        return token;
     }
 }
